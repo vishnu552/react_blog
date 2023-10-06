25cff23a9ef62232a8280973ee3ce693bcdfced5
vishnu suthar <sutharvishnu2021@gmail.com>
2023-10-06 16:35:19 +0530

    login and signup created

---
+++ src/component/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function AuthLayout({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.status);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
+
+export default AuthLayout;
