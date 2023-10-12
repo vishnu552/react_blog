e63ba5d18d75e569ec5f9e591a8b2b918e2b5fdb
vishnu suthar <sutharvishnu2021@gmail.com>
2023-10-12 15:58:38 +0530

    blog notepad added

--- src/component/index.js
+++ src/component/index.js
@@ -3,4 +3,5 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
-export { Header, Footer, Container, Logo, LogoutBtn };
+import RTE from "./RTE";
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
